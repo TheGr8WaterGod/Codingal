3b85222f44fd62961357dbc1910e181d8e2e85d1
TheGr8WaterGod <thamanveerapaneni@gmail.com>
2025-12-25 20:41:21 +1100

    intitial commit

---
+++ M1/Classwork/L1-A1.py
@@ -0,0 +1,12 @@
+#This is a comment
+print("hello, world!")
+#this is a multiple line print
+print("hello world \n this is how you create a new line for one print")
+#this is a performing and printing a simple calculation
+print("the result f 5 + 5 s:", 5 + 5)
+'''
+this is 
+a multiple line comment
+'''
+import keyword
+print(keyword.kwlist)
