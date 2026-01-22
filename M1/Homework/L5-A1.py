23cafb5b3504beebb7187d328ec325249751830b
TheGr8WaterGod <thamanveerapaneni@gmail.com>
2026-01-22 21:07:32 +1100

    initial commit

---
+++ M1/Homework/L5-A1.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the number of times you want to repeat the fibonacci sequence: "))
+x = 0
+y = 1 
+for i in range(0,n):
+    print(x)
+    z = x + y
+    x = y
+    y = z
+#we have to use z as a temporary storage space to store the sum of x and y before actually changing their values
